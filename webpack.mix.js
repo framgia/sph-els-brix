e6958aae9b0560bf85322da3d2daa3e91ec8ef42
Brix Tuyor <brigido.tuyor@sun-asterisk.com>
2022-06-08 13:30:26 +0800

    Finalized setup configurations

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,9 @@
 const mix = require("laravel-mix");
 
-mix.js("resources/js/app.js", "public/js")
+mix.browserSync({
+    proxy: "http://127.0.0.1:8000/",
+});
+
+mix.ts("resources/js/index.tsx", "public/js")
     .react()
     .css("resources/css/app.css", "public/css");
