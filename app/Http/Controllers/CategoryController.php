a00d4ffedcf49eb3cb78cd5a8672603820e74d7d
Brix <105616953+brixtuyor@users.noreply.github.com>
2022-07-05 18:10:51 +0800

    [Backend] Lessons (#25)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public static function get()
-    {
-        return Category::all();
-    }
+	public static function get()
+	{
+		return Category::all();
+	}
+
+	public static function getOne($id)
+	{
+		return Category::where('id', $id);
+	}
 }
