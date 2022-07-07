fdfc473c77e7f9d92e60d7bb23c1dc2fedf416f8
Brix Tuyor <brigido.tuyor@sun-asterisk.com>
2022-07-07 10:37:50 +0800

    Destructured props

--- resources/js/Components/LessonCard.jsx
+++ resources/js/Components/LessonCard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-const LessonCard = (props) => {
+const LessonCard = ({ choices, wordInJapanese, romanizedWord }) => {
 	return (
 		<div className="mt-4 p-4 border rounded grid grid-cols-3 gap-4 place-items-center">
 			<div className="col-span-2 flex flex-col items-center">
-				<p className="font-bold text-4xl">{props.wordInJapanese}</p>
-				<p>({props.romanizedWord})</p>
+				<p className="font-bold text-4xl">{wordInJapanese}</p>
+				<p>({romanizedWord})</p>
 			</div>
 
 			<div className="flex flex-col">
-				{props.choices.map((choice) => (
+				{choices.map((choice) => (
 					<div className="flex items-center gap-4 mb-2" key={choice.id}>
 						<input type="radio" name={choice.word} value={choice.word} />
 						<label>{choice.word}</label>
